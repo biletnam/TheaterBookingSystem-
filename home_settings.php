d869a1404d21ef0039ca3f799053383f48ba8999
FunkyBookends <caspar.nonclercq@gmail.com>
2015-11-18 09:26:42 +0000

    added meta data to template

--- home_settings.php
+++ home_settings.php
@@ -3,4 +3,6 @@
 $page_title = "Home Page Title";
 
 $page_content = "home_content.php";
+
+$page_descrtiption = "Home Page of Caspar's Theatre, come here to see the best shows in town. Online Booking Available NOW!";
 ?>
--- index.php
+++ index.php
@@ -17,6 +17,10 @@ include($page.'_settings.php');
 <html>
 	<head>
 		<title><?php echo $page_title; ?></title>
+		<link rel="stylesheet" href="styles/style.css">
+		<meta name="keywords" content="Caspars Theater, Bookings, Theater, Seats, Shows, Musicals, Plays">
+		<meta name="description" content="<?php echo $page_description; ?>">
+		<meta name="author" content="Caspar Nonclercq">
 	</head>
 	<body>
 
