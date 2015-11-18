afc5433977663f4c959c1fb6e5b52c0fd65219f4
FunkyBookends <caspar.nonclercq@gmail.com>
2015-11-18 15:31:37 +0000

    add in sql files provided

---
+++ database/zone.sql
@@ -0,0 +1,7 @@
+insert into Zone values ('rear stalls', 1.00);
+insert into Zone values ('front stalls', 1.50);
+insert into Zone values ('balcony', 1.80);
+insert into Zone values ('box 1', 5.00);
+insert into Zone values ('box 2', 5.00);
+insert into Zone values ('box 3', 4.00);
+insert into Zone values ('box 4', 4.00);
