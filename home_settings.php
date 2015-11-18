cd72c3cdcc924bdc1368bc552ca560867f662ac3
FunkyBookends <caspar.nonclercq@gmail.com>
2015-11-18 09:27:18 +0000

    fix var typo

--- home_settings.php
+++ home_settings.php
@@ -4,5 +4,5 @@ $page_title = "Home Page Title";
 
 $page_content = "home_content.php";
 
-$page_descrtiption = "Home Page of Caspar's Theatre, come here to see the best shows in town. Online Booking Available NOW!";
+$page_description = "Home Page of Caspar's Theatre, come here to see the best shows in town. Online Booking Available NOW!";
 ?>
