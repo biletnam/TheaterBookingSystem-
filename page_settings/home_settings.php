2eb734b50647088ed7a939d595f06ac05e84fe15
FunkyBookends <caspar.nonclercq@gmail.com>
2015-11-18 21:22:51 +0000

    added code to generate performances

--- page_settings/home_settings.php
+++ page_settings/home_settings.php
@@ -6,4 +6,13 @@ $page_content = "home_content.php";
 
 $page_description = "Home Page of Caspar's Theatre, come here to see the best shows in town. Online Booking Available NOW!";
 
+//gets the performances in the order
+$newest_performances_sql = "
+SELECT DISTINCT pr.* 
+FROM Production pr
+  JOIN Performance pe on pe.title = pr.title
+ORDER BY pe.date_time DESC;
+";
+$newest_performances = $conn->query($newest_performances_sql)
+
 ?>
