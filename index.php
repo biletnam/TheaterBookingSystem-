a5f487f7f334449505a11d0acd2b8cee00e906d1
FunkyBookends <caspar.nonclercq@gmail.com>
2015-11-19 09:58:35 +0000

    changed template stuff

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+$title = "Caspar's Theater - Home Page";
+include 'heading.php';
+$content = '<p>We are a smart company offering printing services at competitive prices.</p>
+		<p>Order soon <a href="order.html">here</a>!';
 
-echo "Hello World";
+include 'template.html';
 
 ?>
